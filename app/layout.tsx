9396a2c10d88b56387776d0f1f97beb5641e40a4
Alex Astro <101653301+alexastro01@users.noreply.github.com>
2024-10-16 12:42:10 +0300

    add anaylitcs

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
 import { Providers } from "@/components/providers";
-
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,6 +17,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={inter.className}>
         <Providers>{children}</Providers>
         <Toaster />
+        <Analytics />
+
       </body>
     </html>
   );
