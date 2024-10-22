0c6fdd4fa189ce393b0fac20c81b6a11f105b07b
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-10-22 13:41:55 +0200

    Improved filters modal ui

--- components/matching/FiltersModal.tsx
+++ components/matching/FiltersModal.tsx
@@ -93,11 +93,12 @@ export default function FiltersModal({ isOpen, onClose, parentFilters, setParent
         <div className="absolute inset-0 bg-card rounded-xl" />
         <div className="relative z-10">
           <div className="flex gap-3 justify-center items-center mb-3">
-            <ToggleRight className="text-primary" size={48} />
-            <h2 className="text-3xl font-bold text-primary">Filters</h2>
+            <ToggleRight className="text-primary sm:h-12 sm:w-12 h-8 w-8" />
+            <h2 className="text-2xl sm:text-3xl font-bold text-primary">Filters</h2>
           </div>
-          <p className="text-xl mb-6 text-primary-foreground">Select and apply the filters you prefer</p>
-          <div className="grid grid-cols-2 gap-4">
+          <p className="text-lg sm:text-xl mb-5 text-primary-foreground">Select and apply the filters you prefer</p>
+          <p className="text-md mb-3 text-primary-foreground text-left">Tags</p>
+          <div className="grid grid-cols-2 gap-2.5">
             {Object.keys(filters.tags).map((tag) => (
               <Button
                 key={tag}
@@ -105,11 +106,14 @@ export default function FiltersModal({ isOpen, onClose, parentFilters, setParent
                   setFilters((prev) => ({ ...prev, tags: { ...prev.tags, [tag as UserTag]: !prev.tags[tag as UserTag] } }))
                 }
                 variant={filters.tags[tag as UserTag] ? "default" : "outline"}
-                className="w-full"
+                className="w-full text-wrap leading-4 p-5"
               >
                 {tag}
               </Button>
             ))}
+          </div>
+          <p className="text-md mt-7 mb-3 text-primary-foreground text-left">Events</p>
+          <div className="grid grid-cols-2 gap-2.5">
             {Object.keys(filters.events).map((event) => (
               <Button
                 key={event}
@@ -123,7 +127,7 @@ export default function FiltersModal({ isOpen, onClose, parentFilters, setParent
                   }))
                 }
                 variant={filters.events[event].selected ? "default" : "outline"}
-                className="w-full"
+                className="w-full text-wrap leading-4 p-5"
               >
                 {filters.events[event].name}
               </Button>
@@ -133,7 +137,7 @@ export default function FiltersModal({ isOpen, onClose, parentFilters, setParent
             onClick={handleModalClose}
             variant="ghost"
             size="icon"
-            className="absolute top-2 right-2 text-primary-foreground hover:text-primary hover:bg-primary/10"
+            className="absolute top-0 right-0 text-primary-foreground hover:text-primary hover:bg-primary/10"
           >
             <X className="h-4 w-4" />
             <span className="sr-only">Close</span>
