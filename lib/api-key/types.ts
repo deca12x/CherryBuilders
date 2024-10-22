342fb22a92108c551ae385d2d94f3543406db91c
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-10-22 13:28:07 +0200

    Removed redundant type

--- lib/api-key/types.ts
+++ lib/api-key/types.ts
@@ -1,9 +1,2 @@
 export type ApiKeyMode = "r" | "w" | "rw";
 export type HttpMethod = "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
-
-export interface ApiKey {
-  id: number;
-  owner_entity: string;
-  mode: ApiKeyMode;
-  key: string;
-}
