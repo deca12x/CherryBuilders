16b4214b63ad8bf45c4cf07355c4ae3a46321861
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-10-29 01:43:18 +0100

    Used filter button component

--- components/matching/NoUsersFound.tsx
+++ components/matching/NoUsersFound.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Filter } from 'lucide-react';
+import React from "react";
+import { Filter } from "lucide-react";
+import FiltersButton from "./FiltersButton";
 
 interface NoUsersFoundProps {
   onOpenFilters: () => void;
@@ -10,13 +11,7 @@ const NoUsersFound: React.FC<NoUsersFoundProps> = ({ onOpenFilters }) => {
     <div className="flex flex-col gap-2 text-center justify-center items-center text-2xl font-bold w-full max-w-xl">
       <span>It seems nobody is here 🤔</span>
       <span className="text-xl">Why don't you try to remove a filter or two?</span>
-      <button
-        className="flex justify-end items-center bg-card rounded-xl py-2 px-3 mt-2"
-        onClick={onOpenFilters}
-      >
-        <Filter className="mr-2 h-5 w-5" />
-        Filters
-      </button>
+      <FiltersButton onOpenFilters={onOpenFilters} />
     </div>
   );
 };
