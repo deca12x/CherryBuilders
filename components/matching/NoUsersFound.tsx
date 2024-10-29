bc671093749c383ee43f52fd5846b9346efad62b
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-10-29 18:53:39 +0100

    Minor change in style

--- components/matching/NoUsersFound.tsx
+++ components/matching/NoUsersFound.tsx
@@ -7,9 +7,9 @@ interface NoUsersFoundProps {
 
 const NoUsersFound: React.FC<NoUsersFoundProps> = ({ onOpenFilters }) => {
   return (
-    <div className="flex flex-col gap-2 text-center justify-center items-center text-2xl font-bold w-full max-w-xl">
+    <div className="flex flex-col gap-1 text-center justify-center items-center text-2xl font-bold w-full max-w-xl p-3">
       <span>It seems nobody is here 🤔</span>
-      <span className="text-xl">Why don't you try to remove a filter or two?</span>
+      <span className="text-xl mb-3">Why don't you try to remove a filter or two?</span>
       <FiltersButton onOpenFilters={onOpenFilters} />
     </div>
   );
