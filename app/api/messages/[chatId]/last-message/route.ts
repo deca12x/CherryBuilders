76c5fab899de1264bbd2aa656bf96e9ff961e0c2
Blackicon <mattiaverdecchi@yahoo.it>
2024-11-11 16:54:26 +0700

    Re added error on empty data array

--- app/api/messages/[chatId]/last-message/route.ts
+++ app/api/messages/[chatId]/last-message/route.ts
@@ -12,17 +12,23 @@ export async function GET(req: NextRequest, { params: { chatId } }: { params: { 
       .select("*")
       .eq("chat_id", chatId)
       .order("created_at", { ascending: false })
-      .limit(1)
-      .single();
+      .limit(1);
 
-    if (error) throw error;
+    if (error) {
+      if (error.code === "PGRST116") {
+        // No message found in database for this chat
+        console.log("No message found in database for this chat");
+        return NextResponse.json({ data }, { status: 404 });
+      }
+      throw error;
+    }
 
     if (!data) {
       console.log("No message found in database for this chat");
       return NextResponse.json({ data }, { status: 404 });
     }
 
-    return NextResponse.json({ data }, { status: 200 });
+    return NextResponse.json({ data: data[0] }, { status: 200 });
   } catch (error) {
     console.error("Error fetching specific chat from database:", error);
     return NextResponse.json({ error: "Error fetching from database" }, { status: 500 });
